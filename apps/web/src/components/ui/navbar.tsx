5304b2e51fb90178336adbb977e5e8a38add1d10
binit <binitgupta.1711@gmail.com>
2026-01-30 02:39:17 +0530

    feat: refactor Navbar component to use Link for navigation

--- apps/web/src/components/ui/navbar.tsx
+++ apps/web/src/components/ui/navbar.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export const Navbar = () => {
   return (
       <div className="flex w-full h-full items-end">
-        <div className="text-2xl -ml-px">Home</div>
-        <div className="text-2xl ml-25">Blogs</div>
-        <div className="text-2xl ml-26.5">Projects</div>
-        <div className="text-2xl ml-20.5">Components</div>
+        <Link href="/" className="text-2xl cursor-pointer -ml-px">Home</Link>
+        <Link href="/blogs" className="text-2xl cursor-pointer ml-25">Blogs</Link>
+        <Link href="/projects" className="text-2xl cursor-pointer ml-26.5">Projects</Link>
+        <Link href="/components" className="text-2xl cursor-pointer ml-20.5">Components</Link>
       </div>
   );
 };
